8d63d4e343156d2e2936950690be1b9c9637c708
kjcaway <whdckstngh@naver.com>
2019-11-16 15:56:49 +0900

    my page template 작성

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import {Router, Route, Switch} from 'react-router-dom';
 import {history} from './store/configureStore';
 
+import MainPage from './pages/temp/MainPageTemp';
 import SignInPage from './pages/temp/SignInPageTemp';
 import SignUpPage from './pages/temp/SignUpPageTemp';
+import MyPage from './pages/temp/MyPageTemp';
 
 const App: React.FC = () => {
   return (
     <Router history={history}>
       <Switch>
         <Route exact path="/" component={() => <div>Hellod</div>}></Route>
+        <Route exact path="/temp/" component={MainPage}></Route>
         <Route exact path="/temp/signin" component={SignInPage}></Route>
         <Route exact path="/temp/signup" component={SignUpPage}></Route>
+        <Route exact path="/temp/:userId" component={MyPage}></Route>
       </Switch>
     </Router>
   );
